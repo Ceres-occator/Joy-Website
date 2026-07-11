dbb5d07bd13fe9bbabf4fbb50cc1d8a61fccab61
Decyrel Dahan <decyreldahan@gmail.com>
2026-07-11 10:27:37 +0800

    middleware to proxy change

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: ['localhost:3000', '127.0.0.1:3000', '192.168.1.17:3000'],
   experimental: {
     // @ts-ignore - necessary if types are not up to date
-    allowedDevOrigins: ['192.168.56.1', 'localhost:3000'],
   } as any, 
   devIndicators: {
     position: 'bottom-left',
